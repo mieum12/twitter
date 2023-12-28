c497f840c3b129c605156e596c21f5cac4656bb4
mieum12 <qpdlqltb1215@gmail.com>
2023-12-28 19:39:07 +0900

    feat: 기본적인 라우팅 시스템 구축

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
+import Layout from "./components/layout";
+import Home from "./routes/home";
+import Profile from "./routes/profile";
+import Login from "./routes/login";
+import CreateAccount from "./routes/create-account";
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Layout/>,
+    children:[
+      {
+        path: '',
+        element: <Home/>
+      },
+      {
+        path: 'profile',
+        element: <Profile/>
+      }
+    ]
+  },
+  {
+    path: '/login',
+    element: <Login/>
+  },
+  {
+    path: '/create-account',
+    element: <CreateAccount/>
+  }
+])
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <>
+    <RouterProvider router={router}/>
+  </>
 }
 
 export default App;
