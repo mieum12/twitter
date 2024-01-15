e8c2b992563c124bf20a542e66f139feb660f236
mieum12 <qpdlqltb1215@gmail.com>
2024-01-15 23:23:41 +0900

    feat: 프로필 내에서 내가 작성한 트윗만 필터링해서 가져오기

--- src/routes/profile.tsx
+++ src/routes/profile.tsx
@@ -1,12 +1,16 @@
-import {auth, storage} from "../firebase";
+import {auth, db, storage} from "../firebase";
 import styled from "styled-components";
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 import {getDownloadURL, ref, uploadBytes} from "firebase/storage";
 import {updateProfile} from "firebase/auth";
+import {collection, getDocs, limit, orderBy, query, where} from "firebase/firestore";
+import {ITweet} from "../components/timeLine";
+import Tweet from "../components/tweet";
 
 export default function Profile(){
   const user = auth.currentUser
   const [avatar, setAvatar] = useState(user?.photoURL)
+  const [tweets,setTweets] = useState<ITweet[]>([])
   const onAvatarChange = async (e:React.ChangeEvent<HTMLInputElement>) => {
     const {files} = e.target
     if (!user) return;
@@ -27,6 +31,32 @@ export default function Profile(){
       })
     }
   }
+
+  const fetchTweets = async () => {
+    // 여기서는 내가 작성한 트윗만 보여주는 쿼리를 만들 것
+    const tweetQuery = query(
+      collection(db,'tweets'),
+      // 트윗의 유저 아이디가 현재 로그인한 유저 아이디와 같은지 필터해주는 함수
+      // 주의: 이런 필터를 사용할 것이라고 firestore에 알려야한다 -> 에러 링크로 가서 색인 생성
+      // 아니면 The query requires an index. 라는 에러가 뜰 것이다
+      where('userId', '==', user?.uid),
+      orderBy('createdAt','desc'),
+      limit(25)
+    );
+    // getDocs에 쿼리를 전달해주면 스냅샷 반환
+    const snapshot = await getDocs(tweetQuery)
+    const tweets = snapshot.docs.map((doc)=>{
+      const {tweet, createdAt, userId, username, photo} = doc.data()
+      return {
+        tweet, createdAt, userId, username, photo,
+        id: doc.id
+      };
+    });
+    setTweets(tweets)
+  }
+  useEffect(()=>{
+    fetchTweets()
+  },[])
   return (
     <Wrapper>
       <AvatarUpload htmlFor='avatar'>
@@ -57,6 +87,11 @@ export default function Profile(){
         */}
         {user?.displayName ?? 'Anonymous' }
       </Name>
+      <Tweets>
+        {tweets.map(tweet => <
+          Tweet key={tweet.id} {...tweet}/>
+        )}
+      </Tweets>
     </Wrapper>
   )
 }
@@ -91,3 +126,10 @@ const AvatarInput = styled.input`
 const Name = styled.span`
   font-size: 22px;
 `;
+
+const Tweets = styled.div`
+  display: flex;
+  width: 100%;
+  flex-direction: column;
+  gap: 10px;
+`
